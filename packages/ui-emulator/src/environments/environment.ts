37aadbc103fd0d9076f2c3f4a36716709a22afbc
Lyubomir Stoychev <71375212+stoychevl@users.noreply.github.com>
2022-05-25 14:56:10 +0300

    - Fixing a bug with loading the right environment config file (#10)
    Signed-off-by: Lyubomir Stoychev <stoychevl@vmware.com>

--- packages/ui-emulator/src/environments/environment.ts
+++ packages/ui-emulator/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import envs from '../../.env/environment.json';
+import envs from '.env/environment.runtime.json';
 
 export const environment = envs;
 /*
